40df4d3633b5dd4ad45922171f9e3ca047398cfe
Durgesh Gupta <durgs7642@gmail.com>
2026-03-28 23:38:42 +0530

    First case study

---
+++ caseStudy1.ts
@@ -0,0 +1,6 @@
+let message: string = "Hello, Durgesh Gupta!";
+let age:number = 22;
+console.log(message,"\nage:", age);
+
+
+//if we try to assign a number to a string variable, it gives error and says that Type 'number' is not assignable to type 'string'. 
