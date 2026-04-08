c8ece31144a590a59671ea162932c864c75901ca
Durgesh Gupta <durgs7642@gmail.com>
2026-04-08 11:50:42 +0530

    case study 2

---
+++ caseStudy2.ts
@@ -0,0 +1,20 @@
+let favoriteFruit : string = "Banana";
+console.log("My favorite fruit is :", favoriteFruit);
+
+function printNumber(num:number): void{
+    console.log("the double value of number is :", num*2);
+}
+printNumber(2);
+
+// this is a single line comment 
+
+/* this is a multi line comment */
+
+class Person{
+     sayHello(): void{
+      console.log("Hello, My name is Durgesh Gupta! ");
+    }
+}
+
+let person1 = new Person();
+person1.sayHello();
